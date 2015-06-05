abf2ec27bca68da615982befe9eccec204800e26
Tyler Faux <tyler@tictail.com>
2015-06-05 16:26:04 -0700

    Add universal header

---
+++ PaymentKit/PaymentKit.h
@@ -0,0 +1,16 @@
+//
+//  PaymentKit.h
+//
+//  Created by Tyler Faux on 03/06/15.
+//  Copyright (c) 2015 Tictail. All rights reserved.
+//
+
+#import "PTKAddressZip.h"
+#import "PTKUSAddressZip.h"
+#import "PTKCard.h"
+#import "PTKCardNumber.h"
+#import "PTKCardCVC.h"
+#import "PTKCardExpiry.h"
+#import "PTKCardType.h"
+#import "PTKComponent.h"
+#import "PTKTextField.h"
